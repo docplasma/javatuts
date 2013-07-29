3d733d5e665b2d6a8956a3478fa9111b23b9bd2b
docplasma <thedocplasma@gmail.com>
2013-07-29 16:36:14 -0500

    Updating to OOP standards. Added Room class. Added ShowRooms class.

---
+++ Arrays/src/FindVacancy.java
@@ -0,0 +1,39 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class FindVacancy {
+
+	public static void main(String[] args) throws IOException {
+		Scanner keyboard = new Scanner(System.in);
+		Scanner diskScanner = new Scanner(new File("src/resources/GuestList.txt"));
+		int guests[] = new int [10];
+		int roomNum;
+		for (roomNum=0; roomNum<10; roomNum++)	{
+			guests[roomNum] = diskScanner.nextInt();
+		}
+		roomNum=0;
+		while (roomNum<10 && guests[roomNum] != 0) {
+			roomNum++;
+		}
+		if (roomNum == 10) {
+			out.println("Sorry, no vcancy");
+			
+		} else {
+			out.print("How many people for room ");
+			out.print(roomNum);
+			out.print("?");
+			guests[roomNum] = keyboard.nextInt();
+			PrintStream listout = new PrintStream("src/resources/GuestList.txt");
+			for (roomNum=0; roomNum<10;roomNum++) {
+				listout.print(guests[roomNum]);
+				listout.print(" ");
+			}
+			
+		}
+
+	}
+
+}
---
+++ Arrays/src/ShowRooms.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class ShowRooms {
+
+	public static void main(String[] args) {
+		Room rooms[];
+		
+
+	}
+
+}
