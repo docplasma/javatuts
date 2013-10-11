41149e3132df6f0b4146fc656d50e8dbeb1781d7
docplasma <thedocplasma@gmail.com>
2013-10-11 18:54:17 -0500

    Implemented

--- Assignement4/src/Graphics/Circle.java
+++ Assignement4/src/Graphics/Circle.java
@@ -1,6 +1,7 @@
 package Graphics;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.util.Random;
 
 public class Circle {
@@ -25,8 +26,58 @@ public class Circle {
 	}
 	
 	//-----Methods
-	public void draw (color) {
-		page.set
+	public void draw (Graphics page) {
+		page.setColor(color);
+		page.fillOval(x, y, diameter, diameter);
+	}
+
+	//-----Getters/Setters
+	public int getDiameter() {
+		return diameter;
+	}
+
+	public void setDiameter(int diameter) {
+		this.diameter = diameter;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public int getxVelocity() {
+		return xVelocity;
+	}
+
+	public void setxVelocity(int xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+
+	public int getyVelocity() {
+		return yVelocity;
+	}
+
+	public void setyVelocity(int yVelocity) {
+		this.yVelocity = yVelocity;
 	}
 
 }
