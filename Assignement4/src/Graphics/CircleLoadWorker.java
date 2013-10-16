ec53384010622bc95fb501cad9dbab7cbf9280cf
docplasma <thedocplasma@gmail.com>
2013-10-15 21:59:26 -0500

    Implementing

--- Assignement4/src/Graphics/CircleLoadWorker.java
+++ Assignement4/src/Graphics/CircleLoadWorker.java
@@ -1,5 +1,26 @@
 package Graphics;
 
-public class CircleLoadWorker {
+import javax.swing.SwingWorker;
 
+public class CircleLoadWorker extends SwingWorker {
+
+	private Circle circle;
+
+	CircleLoadWorker() {
+		
+		circle = new Circle();
+		
+	}
+
+	@Override
+	protected Circle doInBackground() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected void done() {
+		
+		//TODO call paint funciton
+	}
 }
