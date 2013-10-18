03ecb42ae07ed4549e4a70f756e61e3d5b257418
docplasma <thedocplasma@gmail.com>
2013-10-18 00:10:58 -0500

    Parent now JPanel due to repaint performance issues on repainting a JFrame.

---
+++ Assignement4/src/Graphics/CirclePanel.java
@@ -0,0 +1,34 @@
+package Graphics;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class CirclePanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ArrayList<Circle> circles;
+	
+	public CirclePanel(ArrayList<Circle> circles, int width, int height) {
+		this.setPreferredSize(new Dimension(width, height));
+		this.circles = circles;
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		for (int i=0; i < circles.size(); i++) {
+			g2d.setColor(circles.get(i).getColor());
+			g2d.fillOval(circles.get(i).getX(), circles.get(i).getY(), circles.get(i).getDiameter(), circles.get(i).getDiameter());
+		}
+	}	
+}
